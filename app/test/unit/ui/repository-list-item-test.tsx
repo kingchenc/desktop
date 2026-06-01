968a1d7b5b59cb51397b3f8e68021c31fbfd66b5
kingchenc <kingchencp@gmail.com>
2026-06-01 11:53:42 +0200

    test: pass new favorite props to RepositoryListItem in unit test
    The production build type-checks the whole tsconfig program (including tests),
    so the new required isFavorite/onToggleFavorite props must be supplied here too.

--- app/test/unit/ui/repository-list-item-test.tsx
+++ app/test/unit/ui/repository-list-item-test.tsx
@@ -48,6 +48,8 @@ describe('RepositoryListItem', () => {
         matches={noMatches}
         aheadBehind={{ ahead: 2, behind: 1 }}
         changedFilesCount={3}
+        isFavorite={false}
+        onToggleFavorite={() => {}}
       />
     )
 
@@ -72,6 +74,8 @@ describe('RepositoryListItem', () => {
         matches={noMatches}
         aheadBehind={null}
         changedFilesCount={0}
+        isFavorite={false}
+        onToggleFavorite={() => {}}
       />
     )
 
@@ -91,6 +95,8 @@ describe('RepositoryListItem', () => {
         matches={noMatches}
         aheadBehind={null}
         changedFilesCount={0}
+        isFavorite={false}
+        onToggleFavorite={() => {}}
       />
     )
 
